b4a66259067c99120005f6ed1cb623fe9da7c3cb
jiangrui <448421908@qq.com>
2021-12-28 01:01:47 +0800

    build: init projects
    Signed-off-by: jiangrui <448421908@qq.com>

---
+++ scripts/verifyCommit.js
@@ -0,0 +1,17 @@
+// 读取 .git/COMMIT_EDITMSG 文件中的 commit 提交的信息
+const msg = require('fs').readFileSync('.git/COMMIT_EDITMSG', 'utf-8').trim()
+console.log({ msg })
+// 正则校验提交信息的格式
+const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+const mergeRe = /^Merge pull request|Merge branch/
+
+if (!commitRE.test(msg)) {
+	if (!mergeRe.test(msg)) {
+		console.log('git commit信息校验不通过')
+
+		console.error(`git commit的信息格式不对，请按正确格式：title(scope): desc提交`)
+	}
+	process.exit(1)
+} else {
+	console.log('git commit校验通过')
+}

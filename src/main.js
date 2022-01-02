e21005b753580d927669320f09842067c6c588ca
jiangrui <448421908@qq.com>
2022-01-02 23:35:42 +0800

    feat(config): add jest
    Signed-off-by: jiangrui <448421908@qq.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-createApp(App).mount('#app');
+import { createApp } from 'vue'
+import App from './App.vue'
+createApp(App).mount('#app')
 //# sourceMappingURL=main.js.map
+
+function add(x, y) {
+  return x + y
+}
+
